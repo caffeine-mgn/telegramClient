4121dd9dee3ba880620acec7827548465e1a14d8
caffeine-mgn <caffeine.mgn@gmail.com>
2021-03-10 12:57:18 +0300

    fix

--- telegramClient/src/commonMain/kotlin/pw/binom/telegram/dto/LoginUrl.kt
+++ telegramClient/src/commonMain/kotlin/pw/binom/telegram/dto/LoginUrl.kt
@@ -1,5 +1,6 @@
 package pw.binom.telegram.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
@@ -17,14 +18,18 @@ class LoginUrl(
     /**
      * New text of the button in forwarded messages.
      */
-    val forward_text: String?,
+    @SerialName("forward_text")
+    val forwardText: String?,
+
     /**
      * Username of a bot, which will be used for user authorization. See Setting up a bot for more details. If not specified, the current bot's username will be assumed. The url's domain must be the same as the domain linked with the bot. See Linking your domain to the bot for more details.
      */
-    val bot_username: String?,
+    @SerialName("bot_username")
+    val botUsername: String?,
 
     /**
      * Pass True to request the permission for your bot to send messages to the user.
      */
-    val request_write_access: Boolean?
+    @SerialName("request_write_access")
+    val requestWriteAccess: Boolean?
 )
