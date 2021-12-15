9e90b417ec3b94498b12b9d9da6ffb6ecdd754fc
subochev <caffeine.mgn@gmail.com>
2021-12-15 21:36:58 +0300

    Refactoring

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,12 +4,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     }
 }
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
 }
 
 repositories {
@@ -19,11 +19,11 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.6.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-    api("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
-    api("pw.binom:kn-clang:0.1")
-    api("org.jetbrains.kotlin:kotlin-serialization:1.6.0")
+    api("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.6.10")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    api("org.jetbrains.dokka:dokka-gradle-plugin:1.6.0")
+    api("pw.binom:kn-clang:0.1.1")
+    api("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
 }
